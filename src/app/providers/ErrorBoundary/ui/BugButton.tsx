2271f0090329eab79f85b9f3a550848da4c9446d
zaebbb <elik@example.com>
2023-05-16 19:06:38 +0500

    Add UI-eror component, Add ErrorBoundary, Add BugButton, valid errors - refs #22

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Button } from 'shared/ui/Button/Button'
+
+// Тестовый компонент
+export const BugButton: React.FC = () => {
+  const [error, setError] = React.useState<boolean>(false)
+
+  const toggleError = (): void => {
+    setError(true)
+  }
+
+  React.useEffect(() => {
+    if (error) {
+      throw new Error()
+    }
+  }, [error])
+
+  return (
+    <Button onClick={toggleError}>
+      Error
+    </Button>
+  )
+}
