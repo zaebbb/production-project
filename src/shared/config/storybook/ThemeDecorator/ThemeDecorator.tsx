5c4452d7a6610c790b51c14e1a13a841b0d041ab
zaebbb <elik@example.com>
2023-05-23 21:13:18 +0500

    Edit config eslint, add plugin eslint-react-hooks, add new theme btn, add stories new theme btn, edit global styles, add modal ui component, add styles modal, add stories btn, using useCallback, edit component navbar, add createPortla in project, testing project, edit themeDecorator and ThemeProvider, fix all files - refs #29

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -1,8 +1,10 @@
-import { type Theme } from 'app/providers/ThemeProvider'
+import { type Theme, ThemeProvider } from 'app/providers/ThemeProvider'
 import { type Story } from '@storybook/react'
 
 export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => (
-  <div className={`app ${theme}`}>
-    <StoryComponent />
-  </div>
+  <ThemeProvider initialTheme={theme}>
+    <div className={`app ${theme}`}>
+      <StoryComponent />
+    </div>
+  </ThemeProvider>
 )
