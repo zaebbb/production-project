00b4432418a4fbcfeb8a4e1f4553f2262ad2e912
zaebbb <elik@example.com>
2023-07-20 21:32:31 +0500

    Add production - refs #63

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,6 +4,7 @@ import { type BuildOptions } from './types/config'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin'
+import CopyPlugin from 'copy-webpack-plugin'
 
 export function buildPlugins (
   { paths, isDev, apiUrl, project }: BuildOptions
@@ -21,6 +22,11 @@ export function buildPlugins (
       __IS_DEV__: JSON.stringify(isDev),
       __API__: JSON.stringify(apiUrl),
       __PROJECT__: JSON.stringify(project),
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: paths.locales, to: paths.buildLocales },
+      ],
     }),
   ]
 
