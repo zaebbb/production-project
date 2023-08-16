54ac9029e7983b30eccca9226687dd9bfc73dfa6
zaebbb <elik@example.com>
2023-08-16 19:47:08 +0500

    Refactor route config - refs #92

---
+++ src/shared/const/index.ts
@@ -0,0 +1,27 @@
+export enum AppRoutes {
+  MAIN = 'main',
+  ABOUT = 'about',
+  PROFILE = 'profile',
+  ARTICLES = 'articles',
+  ARTICLE_DETAILS = 'article_details',
+  ARTICLE_ADD = 'article_add',
+  ARTICLE_EDIT = 'article_edit',
+  ADMIN_PANEL = 'admin_panel',
+  ACCESS_DENIED = 'access_denied',
+
+  NOT_FOUND = 'not_found'
+}
+
+export const RoutePath: Record<AppRoutes, string> = {
+  [AppRoutes.MAIN]: '/',
+  [AppRoutes.ABOUT]: '/about',
+  [AppRoutes.PROFILE]: '/profile/', // + : id
+  [AppRoutes.ARTICLES]: '/articles',
+  [AppRoutes.ARTICLE_DETAILS]: '/articles/', // + :id
+  [AppRoutes.ARTICLE_ADD]: '/articles/add/',
+  [AppRoutes.ARTICLE_EDIT]: '/articles/:id/edit', // + :id
+  [AppRoutes.ADMIN_PANEL]: '/admin-panel',
+  [AppRoutes.ACCESS_DENIED]: '/access-denied',
+
+  [AppRoutes.NOT_FOUND]: '*',
+}
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -8,10 +8,10 @@ import { LoginModal } from '@/features/AuthByUsername'
 import { getUserAuthData } from '@/entities/User'
 import { Text, TextTheme } from '@/shared/ui/Text/Text'
 import { AppLink, AppLinkTheme } from '@/shared/ui/AppLink/AppLink'
-import { RoutePath } from '@/shared/config/routeConfig/routeConfig'
 import { HStack } from '@/shared/ui/Stack'
 import { NotificationButton } from '@/features/notificationButton'
 import { AvatarMenu } from '@/features/avatarMenu'
+import { RoutePath } from '@/shared/const'
 
 interface NavbarProps {
   className?: string
