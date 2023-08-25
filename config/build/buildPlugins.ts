f9a0f43795afe7b39328bea801a9ba0f3db85ab1
zaebbb <elik@example.com>
2023-08-25 21:04:42 +0500

    Add babel cache, refactoring config webpack and webpack plugins, post install hooks - refs #104

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -8,35 +8,33 @@ import webpack from 'webpack'
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 import { type BuildOptions } from './types/config'
 
-export function buildPlugins (
-  { paths, isDev, apiUrl, project }: BuildOptions
-): webpack.WebpackPluginInstance[] {
+export function buildPlugins ({
+  paths,
+  isDev,
+  apiUrl,
+  project,
+}: BuildOptions): webpack.WebpackPluginInstance[] {
+  const isProd = !isDev
+
   const plugins = [
     new webpack.ProgressPlugin(),
     new HTMLWebpackPlugin({
       template: paths.html,
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].[contenthash:8].css',
-      chunkFilename: 'css/[name].[contenthash:8].css',
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
       __API__: JSON.stringify(apiUrl),
       __PROJECT__: JSON.stringify(project),
     }),
-    new CopyPlugin({
-      patterns: [
-        { from: paths.locales, to: paths.buildLocales },
-      ],
-    }),
   ]
 
   if (isDev) {
-    plugins.push(new BundleAnalyzerPlugin({
-      openAnalyzer: false,
-    }))
     plugins.push(new webpack.HotModuleReplacementPlugin())
+    plugins.push(
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+      })
+    )
     plugins.push(new ReactRefreshPlugin())
     plugins.push(
       new CircularDependencyPlugin({
@@ -53,6 +51,22 @@ export function buildPlugins (
           },
           mode: 'write-references',
         },
+      })
+    )
+  }
+
+  if (isProd) {
+    plugins.push(
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].[contenthash:8].css',
+        chunkFilename: 'css/[name].[contenthash:8].css',
+      })
+    )
+    plugins.push(
+      new CopyPlugin({
+        patterns: [
+          { from: paths.locales, to: paths.buildLocales },
+        ],
       })
     )
   }
