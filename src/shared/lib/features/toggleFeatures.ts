26d88b23089a076e87c36cdb184ca34ef964ea9e
zaebbb <elik@example.com>
2023-09-08 08:33:02 +0500

    Unification work to feature-flags, auto-delete old feature - refs #126

---
+++ src/shared/lib/features/toggleFeatures.ts
@@ -0,0 +1,22 @@
+import { type FeatureFlags } from '../../types/featureFlags'
+import { getFeatureFlags } from './setGetFeatures'
+
+interface ToggleFeatureOptions<T> {
+  name: keyof FeatureFlags
+  on: () => T
+  off: () => T
+}
+
+export const toggleFeatures = <T>(options: ToggleFeatureOptions<T>): T => {
+  const {
+    on,
+    off,
+    name,
+  } = options
+
+  if (getFeatureFlags(name)) {
+    return on()
+  }
+
+  return off()
+}
